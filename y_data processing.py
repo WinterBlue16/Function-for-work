2cecca9db152f147642fa1de037ce32ac0ff5a8a
Winter Lee <leekh090163@gmail.com>
2020-02-06 18:29:16 +0900

    useful

---
+++ y_data processing.py
@@ -0,0 +1,29 @@
+# y 값(target, laber)이 편중되어 있을 경우 사용
+# wine data 예시()
+import pandas as pd
+import numpy as np
+
+# 데이터 읽어들이기
+wine = pd.read_csv("./data/winequality-white.csv", 
+                   sep=";", encoding='utf-8', header=0)
+
+# Input 데이터, Output 데이터로 분리
+y = wine['quality']
+x = wine.drop('quality', axis=1)
+
+y = y.values
+x = x.values
+
+# y 값(=output) 변경
+newlist = []
+for v in list(y):
+    if v <= 4:
+        newlist += [0]
+    elif v <= 7:
+        newlist += [1]
+    else:
+        newlist += [2]
+y = newlist
+
+# y의 column이 더 많은 경우 범주를 더 나눌 필요 있음
+# 이 뒤로는 sklearn을 이용해 train, test set으로 나눠주면 ok
