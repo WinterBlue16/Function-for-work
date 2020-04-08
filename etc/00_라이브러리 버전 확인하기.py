66823beb639dde1f1826c73ec81b583b18f05b58
Winter Lee <leekh090163@gmail.com>
2020-04-08 11:50:20 +0900

    라이브러리 버전 체크

---
+++ etc/00_라이브러리 버전 확인하기.py
@@ -0,0 +1,19 @@
+# 설치된 라이브러리의 버전을 확인하고 싶을 때
+# 방법 1 
+pip freeze 
+
+# 방법 2 
+import urllib.request 
+import keras as k
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import cv2
+
+print('request 버전 :' + urllib.request.__version__)
+print('keras 버전 :' + k.__version__)
+print('tensorflow 버전 :' + tf.__version__)
+print('pandas 버전 :' + pd.__version__)
+print('numpy 버전 :' + np.__version__)
+print('OpenCV 버전 :' + cv2.__version__)
+
