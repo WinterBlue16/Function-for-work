64fdb6e431468d0b2e3a9e49f2075d4cb58f0539
Winter Lee <leekh090163@gmail.com>
2020-04-09 13:33:41 +0900

    zip 파일 압축 풀기

---
+++ etc/11_zip 파일 압축 풀기.py
@@ -0,0 +1,5 @@
+# 대용량 파일(ex.이미지)을 클라우드 또는 colab에서 작업하고 싶을 때
+# 우선 zip 파일을 업로드하고 아래의 코드를 사용한다
+
+!unzip -uq "/content/drive/My Drive/roi.zip(zip 파일 경로)" -d "/content/drive/My Drive/roi_sample(압축 푼 파일 저장할 경로)"
+
