8c447c132325647ca3f3c7bec464e6fa14a352e8
WinterBlue16 <leekh090163@gmail.com>
2020-10-19 23:36:35 +0900

    거꾸로 된 리스트 출력, 저장하기

---
+++ etc/64_거꾸로 된 리스트 출력하기.py
@@ -0,0 +1,11 @@
+
+# 리스트의 순서를 오름차순이나 내림차순이 아니라, 거꾸로 뒤집는 게 편리한 경우도 있다. 
+# 그럴 때 reversed(), reverse() 함수를 활용한다. 
+
+sample_li=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# 리스트를 뒤집어 출력
+print(sample_li.reverse())
+
+# 리스트를 뒤집어 출력 2
+print(list(reversed(sample_li)))
