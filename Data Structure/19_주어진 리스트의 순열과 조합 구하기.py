b9183f7000fc5e61eb7ce1eee85a945896281758
WinterBlue16 <leekh090163@gmail.com>
2021-02-25 12:13:06 +0900

    .\Data Structure\19_주어진 리스트의 순열과 조합 구하기.py 수정: product와 combinations_with_replacement 차이 설명 추가

--- Data Structure/19_주어진 리스트의 순열과 조합 구하기.py
+++ Data Structure/19_주어진 리스트의 순열과 조합 구하기.py
@@ -14,7 +14,7 @@ per_1=list(permutations(li_1)) # li_2, li_3
 per_2=list(permutations(li_1, 2)) # N=2
 # print(per_2)
 
-# 각 리스트 나열 시 N개만큼 뽑아 나열하는 모든 경우의 수 출력(중복 허용)
+# 각 리스트 나열 시 N개만큼 뽑아 나열하는 모든 경우의 수 출력(순서 고려함, 중복 허용)
 per_3=list(product(li_1, repeat=2)) # li_2, li_3
 # print(per_3)
 
@@ -29,9 +29,9 @@ con_2=list(combinations_with_replacement(li_1, 2)) # li_2, li_3
 # 3개의 리스트에서 각 리스트 값들을 나열할 때의 모든 경우의 수
 all_li=[li_1,li_2,li_3]
 all_per=list(product(*all_li))
-# print(all_per)
+print(all_per)
 
 # 2개의 리스트로 모든 경우의 수
-two_li=[li_1,li_2]
+two_li=[li_2,li_3]
 two_per=list(product(*two_li))
-print(two_per)
+# print(two_per)
