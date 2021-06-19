03ac9e16fb80f570e92779dba5ad4a04261e0af1
WinterBlue16 <leekh090163@gmail.com>
2021-06-19 23:09:34 +0900

    50_거꾸로\ 된\ 리스트\ 출력하기.py 이동 to Data Structure/list/

--- etc/14_json 데이터 불러오기.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-# example_json.json이라는 파일을 불러와 읽고 싶을 때
-example = pd.read_json('./example_json.json', typ='frame', encoding='utf-8')
--- etc/60_json 파일 로드하기.py
+++
@@ -1,7 +0,0 @@
-# 저장된 json 파일을 로드한다.
-import json
-
-file_path = 'json 파일 저장 경로'
-
-with open(file_path, 'r') as json_file:
-    json_to_dict = json.load(json_file)  # json이 dict로 바뀌어 로드된다.
