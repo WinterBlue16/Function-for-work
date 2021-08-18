aec39616d4f9522dd2baf87e11f49bb46a82408e
WinterBlue16 <leekh090163@gmail.com>
2021-08-19 00:26:47 +0900

    etc/61_해당 변수가 코드 내에 존재하는지 확인.py 수정: 함수 내 존재하는 함수 확인 추가

--- etc/61_해당 변수가 코드 내에 존재하는지 확인.py
+++ etc/61_해당 변수가 코드 내에 존재하는지 확인.py
@@ -1,12 +1,32 @@
 # 변수가 내가 작성한 코드 내에 존재하는지 확인하고 싶을 때
+# 변수 생성
 my_variable = 'my variable'
+
+# 함수 생성
+
+
+def function():
+    global y
+    y = 1
+    # if 'y' in locals():
+    #     print(True)
+    return y
+
+
+function()
 
 # in local
 if 'my_variable' in locals():
     print(True)
 
+if 'y' in locals():
+    print(True)
+
 # in global
 if 'my_variable' in globals():
+    print(True)
+
+if 'y' in globals():
     print(True)
 
 # catch nameerror
@@ -15,3 +35,6 @@ try:
     print(my_variable)
 except NameError as e:
     my_variable = None
+
+
+# 함수 내부에서 global로 정의했을 경우 locals()에서는 False가 나온다. 하지만 함수 바깥에서는 locals(), globals() 전부 True값이다.
