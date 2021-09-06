051817a5444619e1a9c8b0ff1cee264e4256397f
WinterBlue16 <leekh090163@gmail.com>
2021-09-07 00:18:14 +0900

    .\etc\66_긴 문장 한꺼번에 string으로 저장하기.py 생성

---
+++ etc/66_긴 문장 한꺼번에 string으로 저장하기.py
@@ -0,0 +1,21 @@
+# 단순한 한두 문장이라면 쉽게 string으로 만들 수 있지만, 긴 문단이나 줄바꿈이 되어 있는 텍스트를 string으로 바꾸는 것은\
+# 번거롭다. 하지만 아래와 같은 방법으로 그런 문장도 간단히 string으로 만들 수 있다.
+
+sample_text = '''직장생활의 고민을 나누고 성장하고 싶은 사람들과 함께 도전하고 싶다면?
+👉 https://bit.ly/3z6YYHo
+
+직장생활에 지친 직장인 필독서 [JOY AT WORK]
+👉 https://bit.ly/3mSlxgg
+
+지금 당장 퇴사하고 더 좋은 회사로 이직하고 싶다면?
+👉 https://bit.ly/2QDHGBh
+
+퇴사한 이형 구독자 형팸들과 소통하고 싶다면?
+👉 http://bit.ly/2wVLrHX (참여코드: 0214)
+
+* 비즈니스 문의: official@alivecommunity.co.kr
+* 직장생활 고민 사연 : https://forms.gle/4y3cRsjQQ6V9dCxv5
+
+'''
+
+print(sample_text)
