e68276e4b5c08672b2f504d2eea13dbc7feb695d
WinterBlue16 <leekh090163@gmail.com>
2022-01-29 00:14:38 +0900

    docstring 함수 정의 추가

--- etc/79_python으로 api 호출하기.py
+++ etc/79_python으로 api 호출하기.py
@@ -5,6 +5,15 @@ import requests
 
 
 def request_get_api(api_key, url):
+    """get api를 호출합니다.
+
+    Args:
+        api_key ([string]): [request 호출에 필요한 api key입니다. api key가 없을 경우 401 에러가 발생합니다.]
+        url ([string]): [request를 보낼 api의 주소입니다.]
+
+    Returns:
+        [dictionary]: [api 호출 결과로 response된 json 데이터입니다.]
+    """
     headers = {
         "Authorization": "Bearer {}".format(api_key),
         "Content-Type": "application/json"
@@ -17,6 +26,16 @@ def request_get_api(api_key, url):
 
 
 def request_post_api(api_key, raw_data, url):
+    """post api를 호출합니다. 
+
+    Args:
+        api_key ([string]): [request 호출에 필요한 api key입니다. api key가 없을 경우 401 에러가 발생합니다.]
+        raw_data ([dictionary]): [post로 생성할 데이터입니다.]
+        url ([string]): [request를 보낼 api의 주소입니다.]
+
+    Returns:
+        [dictionary]: [api 호출 결과로 response된 json 데이터입니다.]
+    """
     headers = {
         "Authorization": "Bearer {}".format(api_key),
         "Content-Type": "application/json"
