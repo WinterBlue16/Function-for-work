b69d9210cc0a8c2e51f7dec6d9dd83d9040cc7f1
WinterBlue16 <leekh090163@gmail.com>
2022-01-29 00:02:15 +0900

    api post 호출하기

--- etc/79_python으로 api 호출하기.py
+++ etc/79_python으로 api 호출하기.py
@@ -1,6 +1,8 @@
 import json
 import requests
 
+# GET
+
 
 def request_get_api(api_key, url):
     headers = {
@@ -9,4 +11,15 @@ def request_get_api(api_key, url):
     }
 
     response = requests.get(url, headers=headers).json()
+    return response
+
+# POST
+
+
+def request_post_api(api_key, raw_data, url):
+    headers = {
+        "Authorization": "Bearer {}".format(api_key),
+        "Content-Type": "application/json"
+    }
+    response = requests.post(url, headers=headers, json=raw_data).json()
     return response
