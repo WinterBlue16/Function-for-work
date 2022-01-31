8174fc9a887b41ab0e5f114898545035b369685c
WinterBlue16 <leekh090163@gmail.com>
2022-02-01 00:02:05 +0900

    add example

--- Data Structure/json/69_string(json) to json.py
+++ Data Structure/json/69_string(json) to json.py
@@ -4,3 +4,8 @@ import json
 def string_json_to_json(string_json):
     convert_json = json.loads(string_json)
     return convert_json
+
+
+sample = '{"sample": "apple", "sample2":"strawberry"}'
+print(string_json_to_json(sample))
+print(type(string_json_to_json(sample)))
