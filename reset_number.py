d3cb3996f9ab570ab88de2f07957f1c2116c2496
WinterBlue16 <leekh090163@gmail.com>
2022-02-05 00:45:14 +0900

    번호 초기화 및 재정렬

--- reset_number.py
+++ reset_number.py
@@ -10,10 +10,12 @@ def reset_idx_in_dir(dir_path):
     i = 1
     for filename in os.listdir(dir_path):
         if filename.endswith('py') or filename.endswith('ipynb'):
-            print(filename)
+            base_file_name = filename[filename.index('_'):]
             # 반드시 경로 추가
-            os.rename(dir_path+'/'+filename, dir_path+'/'+str(i)+filename[2:])
+            os.rename(dir_path+'/'+filename, dir_path +
+                      '/'+str(i)+base_file_name)
             i += 1
 
 
-reset_idx_in_dir('C:/Users/CUCHO/Function-for-work/etc')
+# TODO: 번호가 붙어있지 않은 파일들에 번호 붙이는 함수 생성
+# reset_idx_in_dir('C:/Users/CUCHO/Function-for-work/Data Structure/tuple')
