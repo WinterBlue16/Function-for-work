3dcfdf70a73e0090392035e2d5c088be1bd7e782
WinterBlue16 <leekh090163@gmail.com>
2022-02-15 00:02:03 +0900

    example 추가

--- etc/43_hex_이중_해제.py
+++ etc/43_hex_이중_해제.py
@@ -1,4 +1,5 @@
+from genericpath import samefile
 from urllib.parse import unquote
 
-sample = ''
-print(unquote(unquote()))
+sample = 'videoplayback%253Fexpire%253D1532566750%2526mime%253Dvideo%25252Fmp4%2526key%253Dyt6%2526mt%253D1532544983%2526fvip%253D5%2526i'
+print(unquote(unquote(sample)))
