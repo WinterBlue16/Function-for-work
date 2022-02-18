051d8864e387e0e89acf5e4e92b7e60ca5301eab
WinterBlue16 <leekh090163@gmail.com>
2022-02-18 09:03:47 +0900

    json 가독성 좋게 출력하기

--- Data Structure/json/5_string(json) to json.py
+++ Data Structure/json/5_string(json) to json.py
@@ -6,6 +6,14 @@ def string_json_to_json(string_json):
     return convert_json
 
 
+def print_json_indent(string_json):
+    convert_json = json.loads(string_json)
+    pretty_json = json.dumps(convert_json, indent=4)
+    print(pretty_json)
+    return pretty_json
+
+
 sample = '{"sample": "apple", "sample2":"strawberry"}'
 print(string_json_to_json(sample))
 print(type(string_json_to_json(sample)))
+print(print_json_indent(sample))
