3036aeebbe193ebaac744224620684c1c2263113
WinterBlue16 <leekh090163@gmail.com>
2022-04-01 23:48:06 +0900

    for loop if else in list comprehension

---
+++ etc/45_for loop & if else in list comprehension.py
@@ -0,0 +1,10 @@
+sample_list = [i for i in range(10)]
+
+# for loop
+print([i*2 for i in sample_list])
+
+# if else + for loop
+print([i for i in sample_list if i % 2 == 0])
+
+# if else + for loop
+print([i*2 if i % 2 == 0 else i for i in sample_list])
