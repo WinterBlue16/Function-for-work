851c0f137e569fffe79c776647128d109976d405
WinterBlue16 <leekh090163@gmail.com>
2022-04-07 00:01:11 +0900

    [mod]remarks

--- etc/45_for loop & if else in list comprehension.py
+++ etc/45_for loop & if else in list comprehension.py
@@ -3,7 +3,7 @@ sample_list = [i for i in range(10)]
 # for loop
 print([i*2 for i in sample_list])
 
-# if else + for loop
+# if(without else) + for loop
 print([i for i in sample_list if i % 2 == 0])
 
 # if else + for loop
