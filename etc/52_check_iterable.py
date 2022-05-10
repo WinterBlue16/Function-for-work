87bb7ae8a51ce1138a11e0d94e83bf4667badcac
Winter <winter@glocalizeinc.com>
2022-05-11 00:03:15 +0900

    [add]examples

--- etc/52_check_iterable.py
+++ etc/52_check_iterable.py
@@ -1,6 +1,31 @@
 from collections.abc import Iterable
 
 sample_object = {'a':1, 'b':2}
+sample_tuple = ('a', 'b')
+sample_list = ['a', 'b']
+sample_string = 'ab'
+sample_int = 1
 
+# dict
 if isinstance(sample_object, Iterable):
     print(True)
+
+# tuple
+if isinstance(sample_tuple, Iterable):
+    print(True)
+    
+# list
+if isinstance(sample_tuple, Iterable):
+    print(True)
+
+# string
+if isinstance(sample_string, Iterable):
+    print(True)
+else:
+    print(False)
+
+# int
+if isinstance(sample_int, Iterable):
+    print(True)
+else:
+    print(False)
